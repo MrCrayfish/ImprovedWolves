bf0fe9b3b2c0e56526c3d3fcf383e7abbb076fbb
MrCrayfish <mrcrayfish@hotmail.com>
2020-02-05 23:16:43 +1030

    Forgot to include in last commit

--- src/main/java/com/mrcrayfish/improvedwolves/tileentity/DogBowlTileEntity.java
+++ src/main/java/com/mrcrayfish/improvedwolves/tileentity/DogBowlTileEntity.java
@@ -1,28 +1,50 @@
 package com.mrcrayfish.improvedwolves.tileentity;
 
 import com.mrcrayfish.improvedwolves.init.ModTileEntities;
+import com.mrcrayfish.improvedwolves.inventory.container.DogBowlContainer;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 import javax.annotation.Nullable;
 
 /**
  * Author: MrCrayfish
  */
-public class DogBowlTileEntity extends TileEntity
+public class DogBowlTileEntity extends BasicLootTileEntity
 {
     public DogBowlTileEntity()
     {
         super(ModTileEntities.DOG_BOWL);
     }
 
-
     @Override
     public void read(CompoundNBT compound)
     {
         super.read(compound);
+    }
+
+    @Override
+    public int getSizeInventory()
+    {
+        return 1;
+    }
+
+    @Override
+    protected ITextComponent getDefaultName()
+    {
+        return new TranslationTextComponent("container.improvedwolves.dog_bowl");
+    }
+
+    @Override
+    protected Container createMenu(int windowId, PlayerInventory playerInventory)
+    {
+        return new DogBowlContainer(windowId, playerInventory, this);
     }
 
     @Override
